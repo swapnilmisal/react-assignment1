4f540c3f21435921e8b21dfc667431606c094ef8
Swapnil Misal <swapnil.misal@haptik.ai>
2020-06-28 13:06:08 +0530

    Updates

---
+++ precache-manifest.87050bd5bb5a0f9d63e208140b0e16ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a571eba7ff653182478f73b3b3004048",
+    "url": "/react-assignment1/index.html"
+  },
+  {
+    "revision": "a902b9b2aae0f9a94953",
+    "url": "/react-assignment1/static/css/main.70ba33b4.chunk.css"
+  },
+  {
+    "revision": "79df5874abdf618ba3a5",
+    "url": "/react-assignment1/static/js/2.4981432c.chunk.js"
+  },
+  {
+    "revision": "a902b9b2aae0f9a94953",
+    "url": "/react-assignment1/static/js/main.bc780200.chunk.js"
+  },
+  {
+    "revision": "71cc3091baad1d380a14",
+    "url": "/react-assignment1/static/js/runtime-main.b558aea2.js"
+  }
+]);
